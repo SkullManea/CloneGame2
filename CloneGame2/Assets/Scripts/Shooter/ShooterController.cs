713750d18fb4c2a003a24fe53af856ab85605a3f
Jayaveer Harilal <jjharilal@gmail.com>
2026-08-12 11:04:25 +0200

    Added 2D shooter aiming system

--- CloneGame2/Assets/Scripts/Shooter/ShooterController.cs
+++ CloneGame2/Assets/Scripts/Shooter/ShooterController.cs
@@ -1,6 +1,43 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class ShooterController
+public class ShooterController : MonoBehaviour
 {
-    
+    [Header("Shooter References")]
+    [SerializeField] private Transform pivot;
+    [SerializeField] private Transform muzzlePoint;
+
+    private Camera mainCamera;
+
+    private void Awake()
+    {
+        mainCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        AimAtPointer();
+    }
+
+    private void AimAtPointer()
+    {
+        if (Pointer.current == null)
+            return;
+
+        Vector2 pointerPosition =
+            Pointer.current.position.ReadValue();
+
+        Vector3 worldPosition =
+            mainCamera.ScreenToWorldPoint(pointerPosition);
+
+        Vector2 direction =
+            worldPosition - pivot.position;
+
+        float angle =
+            Mathf.Atan2(direction.y, direction.x)
+            * Mathf.Rad2Deg;
+
+        pivot.rotation =
+            Quaternion.Euler(0f, 0f, angle);
+    }
 }
